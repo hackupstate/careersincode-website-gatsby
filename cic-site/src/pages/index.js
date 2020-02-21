51ef5a7087478cf0a757946847f56769c6590759
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2020-02-21 13:04:48 -0500

    Added comments to footer.js and triplesquare.js with to-do notes, created Testimonials component and added to index.js, created testimonials.js and used Reactstrap Carousel to set up rotating testimonials, added students, team, and instructors folders to src/images, added kellycorey.jpg to students, joeybuczek.jpg to instructors, and dougcrescenzi.png to team, imported kelly, doug, and joey to testimonials.js, converted text from testimonial section on index.html to testimonials.js

--- cic-site/src/pages/index.js
+++ cic-site/src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Testimonials from '../components/testimonials'
 import TripleSquare from '../components/triplesquare'
 
 const IndexPage = () => (
@@ -15,6 +16,7 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
     <br />
     <Link to="/alumni/">Alumni Page</Link>
+    <Testimonials />
     <TripleSquare />
   </Layout>
 )
